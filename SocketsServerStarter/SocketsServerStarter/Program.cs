3b22a429cf00672f113a42dea3eb1095410caf00
hemakshi_dev <hemakshi@gtropy.com>
2023-08-24 14:51:29 +0530

    Updated

--- SocketsServerStarter/SocketsServerStarter/Program.cs
+++ SocketsServerStarter/SocketsServerStarter/Program.cs
@@ -13,40 +13,60 @@ namespace SocketsServerStarter
     {
         public static void Main(string[] args)
         {
+            // Create a socket that listens for incoming connections
             Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
+            // Bind the socket to an IP address and port
             IPAddress ipaddr = IPAddress.Any;
-
             IPEndPoint ipep = new IPEndPoint(ipaddr, 23000);
-
             listenerSocket.Bind(ipep);
 
+            // Start listening for incoming connections with a maximum backlog of 5
             listenerSocket.Listen(5);
 
             Console.WriteLine("About to accept the incoming connection");
 
+            // Accept an incoming connection
             Socket client = listenerSocket.Accept();
-
             Console.WriteLine("client connected"+ client.ToString() + " - IP End point" + client.RemoteEndPoint.ToString());
 
+            // Receive data from the client
+       
             byte[] buff = new byte[128];
-
             int numberOfReceivedBytes = 0;
 
-            numberOfReceivedBytes = client.Receive(buff);
+            while (true)
+            {
 
-            Console.WriteLine("Number of received bytes " + numberOfReceivedBytes);
+                numberOfReceivedBytes = client.Receive(buff);
 
-            Console.WriteLine("Data sent by client is: " + buff);
+                Console.WriteLine("Number of received bytes " + numberOfReceivedBytes);
 
-            // Conversion of byte array to human readable string
+                Console.WriteLine("Data sent by client is: " + buff);
 
-            string receivedText = Encoding.ASCII
-                .GetString(buff, 0, numberOfReceivedBytes);
+                // Convert received bytes to a human-readable string
+                string receivedText = Encoding.ASCII
+                    .GetString(buff, 0, numberOfReceivedBytes);
 
-            Console.WriteLine("Data sent by client is: " + receivedText);
+                Console.WriteLine("Data sent by client is: " + receivedText);
 
+                client.Send(buff);
+
+                if(receivedText == "x")
+                {
+                    break;
+                }
+
+                Array.Clear(buff, 0, buff.Length);
+                numberOfReceivedBytes = 0;
+            }
             Console.ReadLine();
+
+            // Close the client socket when done
+            client.Close();
+
+            // Close the listener socket when done
+            listenerSocket.Close();
 
         }
     }
